a374a5e2a2910a4461b74780753e196fbe76a656
Ana Caroll <ana2417carollina@gmail.com>
2025-12-04 17:35:29 -0300

    feat(front): instalando bibliotecas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
+import { provideEventPlugins } from "@taiga-ui/event-plugins";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideAnimations(), provideRouter(routes), provideAnimationsAsync(), provideEventPlugins()]
 };
