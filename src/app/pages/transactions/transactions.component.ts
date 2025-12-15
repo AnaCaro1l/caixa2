4767a450978b5e5180cae40ed2aabf63830f19ce
Ana Caroll <ana2417carollina@gmail.com>
2025-12-15 16:54:08 -0300

    fix(front): ajustando o checkbox da tabela de transações

--- src/app/pages/transactions/transactions.component.ts
+++ src/app/pages/transactions/transactions.component.ts
@@ -7,9 +7,20 @@ import {
   TuiDataList,
 } from '@taiga-ui/core';
 import { FiltersComponent } from '../../shared/filters/filters.component';
-import { TuiTable } from '@taiga-ui/addon-table';
+import { TuiTable, TuiTableControl } from '@taiga-ui/addon-table';
 import { TuiCell } from '@taiga-ui/layout';
-import { TuiCheckbox } from '@taiga-ui/kit';
+import { TuiBadge, TuiCheckbox, TuiStatus } from '@taiga-ui/kit';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+
+
+interface Transaction {
+  id: number;
+  updateAt: string;
+  value: number;
+  status: 'paid' | 'pending' | 'canceled';
+  dueDate: string;
+  companyId: number;
+}
 
 @Component({
   selector: 'app-transactions',
@@ -24,15 +35,60 @@ import { TuiCheckbox } from '@taiga-ui/kit';
     TuiDropdown,
     TuiDataList,
     TuiCheckbox,
+    CurrencyPipe,
+    TuiBadge,
+    TuiStatus,
+    DatePipe,
+    TuiTableControl,
   ],
   templateUrl: './transactions.component.html',
-  styleUrl: './transactions.component.scss',
+  styleUrls: ['./transactions.component.scss'],
 })
 export class TransactionsComponent {
   value = '';
   allchecked = false;
   dropdownOpen = false;
   open = false;
+  checked = false;
+  selected = [];
 
-  
+  transactions: Transaction[] = [
+    {
+      id: 1,
+      updateAt: '2024-01-01',
+      value: 100.0,
+      status: 'paid',
+      dueDate: '2024-01-10',
+      companyId: 1,
+    },
+    {
+      id: 2,
+      updateAt: '2024-01-02',
+      value: 200.0,
+      status: 'pending',
+      dueDate: '2024-01-15',
+      companyId: 2,
+    },
+    {
+      id: 3,
+      updateAt: '2024-01-03',
+      value: 150.0,
+      status: 'canceled',
+      dueDate: '2024-01-20',
+      companyId: 1,
+    },
+  ];
+
+  getAppearance(status: Transaction['status']): 'success' | 'warning' | 'error' {
+    switch (status) {
+      case 'paid':
+        return 'success';
+      case 'pending':
+        return 'warning';
+      case 'canceled':
+        return 'error';
+      default:
+        return 'success';
+    }
+  }
 }
